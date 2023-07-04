70497b2fd703d55207ec76a9be44fc4ea88ebbe3
Jørgen S. Dokken <dokken@simula.no>
2023-07-04 11:40:50 +0200

    Try fixing subdomains

--- chapter3/subdomains.py
+++ chapter3/subdomains.py
@@ -216,7 +216,7 @@ def create_mesh(mesh, cell_type, prune_z=False):
     cells = mesh.get_cells_type(cell_type)
     cell_data = mesh.get_cell_data("gmsh:physical", cell_type)
     points = mesh.points[:, :2] if prune_z else mesh.points
-    out_mesh = meshio.Mesh(points=points, cells={cell_type: cells}, cell_data={"name_to_read": [cell_data]})
+    out_mesh = meshio.Mesh(points=points, cells={cell_type: cells}, cell_data={"name_to_read": [cell_data.astype(np.int32]})
     return out_mesh
 
 
