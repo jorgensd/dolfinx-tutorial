5110f4c6bc52b3e04a25eda780057289690ff929
Jørgen S. Dokken <dokken@simula.no>
2025-01-06 09:56:20 +0100

    Sync jupytext

--- chapter2/linearelasticity_code.py
+++ chapter2/linearelasticity_code.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.16.4
+#       jupytext_version: 1.16.6
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -14,17 +14,24 @@
 # ---
 
 # # Implementation
+#
 # Author: Jørgen S. Dokken
 #
 # In this tutorial, you will learn how to:
+#
 # - Use a vector function space
 # - Create a constant boundary condition on a vector space
-# - Visualize cell wise constant functions
+# - Visualize cell-wise constant functions
 # - Compute Von Mises stresses
 #
 # ## Test problem
-# As a test example, we will model a clamped beam deformed under its own weigth in 3D. This can be modeled, by setting the right-hand side body force per unit volume to $f=(0,0,-\rho g)$ with $\rho$ the density of the beam and $g$ the acceleration of gravity. The beam is box-shaped with length $L$ and has a square cross section of width $W$. We set $u=u_D=(0,0,0)$ at the clamped end, x=0. The rest of the boundary is traction free, that is, we set $T=0$. We start by defining the physical variables used in the program.
+#
+# As a test example, we will model a clamped beam deformed under its own weight in 3D.
+# This can be modeled, by setting the right-hand side body force per unit volume to $f=(0,0,-\rho g)$ with $\rho$ the density of the beam and $g$ the acceleration of gravity. The beam is box-shaped with length $L$ and has a square cross section of width $W$. We set $u=u_D=(0,0,0)$ at the clamped end, x=0. The rest of the boundary is traction free, that is, we set $T=0$.
+# We start by defining the physical variables used in the program.
+#
 
+# +
 # Scaled variable
 import pyvista
 from dolfinx import mesh, fem, plot, io, default_scalar_type
@@ -32,6 +39,7 @@ from dolfinx.fem.petsc import LinearProblem
 from mpi4py import MPI
 import ufl
 import numpy as np
+
 L = 1
 W = 0.2
 mu = 1
@@ -41,18 +49,26 @@ gamma = 0.4 * delta**2
 beta = 1.25
 lambda_ = beta
 g = gamma
+# -
 
 # We then create the mesh, which will consist of hexahedral elements, along with the function space.
 # As we want a vector element with three compoenets, we add `(3, )` or `(domain.geometry.dim, )` to the element tuple to make it a triplet
 # However, we also could have used `basix.ufl`s functionality, creating a vector element `element = basix.ufl.element("Lagrange", domain.topology.cell_name(), 1, shape=(domain.geometry.dim,))`, and intitializing the function space as `V = dolfinx.fem.functionspace(domain, element)`.
+#
 
-domain = mesh.create_box(MPI.COMM_WORLD, [np.array([0, 0, 0]), np.array([L, W, W])],
-                         [20, 6, 6], cell_type=mesh.CellType.hexahedron)
-V = fem.functionspace(domain, ("Lagrange", 1, (domain.geometry.dim, )))
+domain = mesh.create_box(
+    MPI.COMM_WORLD,
+    [np.array([0, 0, 0]), np.array([L, W, W])],
+    [20, 6, 6],
+    cell_type=mesh.CellType.hexahedron,
+)
+V = fem.functionspace(domain, ("Lagrange", 1, (domain.geometry.dim,)))
 
 
 # ## Boundary conditions
+#
 # As we would like to clamp the boundary at $x=0$, we do this by using a marker function, which locate the facets where $x$ is close to zero by machine precision.
+#
 
 # +
 def clamped_boundary(x):
@@ -67,20 +83,25 @@ bc = fem.dirichletbc(u_D, fem.locate_dofs_topological(V, fdim, boundary_facets),
 # -
 
 # As we want the traction $T$ over the remaining boundary to be $0$, we create a `dolfinx.Constant`
+#
 
 T = fem.Constant(domain, default_scalar_type((0, 0, 0)))
 
 # We also want to specify the integration measure $\mathrm{d}s$, which should be the integral over the boundary of our domain. We do this by using `ufl`, and its built in integration measures
+#
 
 ds = ufl.Measure("ds", domain=domain)
 
 
 # ## Variational formulation
+#
 # We are now ready to create our variational formulation in close to mathematical syntax, as for the previous problems.
+#
 
 # +
 def epsilon(u):
-    return ufl.sym(ufl.grad(u))  # Equivalent to 0.5*(ufl.nabla_grad(u) + ufl.nabla_grad(u).T)
+    """Equivalent to 0.5*(ufl.nabla_grad(u) + ufl.nabla_grad(u).T)"""
+    return ufl.sym(ufl.grad(u))
 
 
 def sigma(u):
@@ -104,14 +125,20 @@ L = ufl.dot(f, v) * ufl.dx + ufl.dot(T, v) * ds
 # ```
 #
 # ## Solve the linear variational problem
+#
 # As in the previous demos, we assemble the matrix and right hand side vector and use PETSc to solve our variational problem
+#
 
-problem = LinearProblem(a, L, bcs=[bc], petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+problem = LinearProblem(
+    a, L, bcs=[bc], petsc_options={"ksp_type": "preonly", "pc_type": "lu"}
+)
 uh = problem.solve()
 
 # ## Visualization
+#
 
 # As in the previous demos, we can either use Pyvista or Paraview for visualization. We start by using Pyvista. Instead of adding scalar values to the grid, we add vectors.
+#
 
 # +
 pyvista.start_xvfb()
@@ -136,6 +163,7 @@ else:
 # We could also use Paraview for visualizing this.
 # As explained in previous sections, we save the solution with `XDMFFile`.
 # After opening the file `deformation.xdmf` in Paraview and pressing `Apply`, one can press the `Warp by vector button` ![Warp by vector](warp_by_vector.png) or go through the top menu (`Filters->Alphabetical->Warp by Vector`) and press `Apply`. We can also change the color of the deformed beam by changing the value in the color menu ![color](color.png) from `Solid Color` to `Deformation`.
+#
 
 with io.XDMFFile(domain.comm, "deformation.xdmf", "w") as xdmf:
     xdmf.write_mesh(domain)
@@ -143,19 +171,23 @@ with io.XDMFFile(domain.comm, "deformation.xdmf", "w") as xdmf:
     xdmf.write_function(uh)
 
 # ## Stress computation
+#
 # As soon as the displacement is computed, we can compute various stress measures. We will compute the von Mises stress defined as $\sigma_m=\sqrt{\frac{3}{2}s:s}$ where $s$ is the deviatoric stress tensor $s(u)=\sigma(u)-\frac{1}{3}\mathrm{tr}(\sigma(u))I$.
+#
 
-s = sigma(uh) - 1. / 3 * ufl.tr(sigma(uh)) * ufl.Identity(len(uh))
-von_Mises = ufl.sqrt(3. / 2 * ufl.inner(s, s))
+s = sigma(uh) - 1.0 / 3 * ufl.tr(sigma(uh)) * ufl.Identity(len(uh))
+von_Mises = ufl.sqrt(3.0 / 2 * ufl.inner(s, s))
 
 # The `von_Mises` variable is now an expression that must be projected into an appropriate function space so that we can visualize it. As `uh` is a linear combination of first order piecewise continuous functions, the von Mises stresses will be a cell-wise constant function.
+#
 
 V_von_mises = fem.functionspace(domain, ("DG", 0))
 stress_expr = fem.Expression(von_Mises, V_von_mises.element.interpolation_points())
 stresses = fem.Function(V_von_mises)
 stresses.interpolate(stress_expr)
 
-# In the previous sections, we have only visualized first order Lagrangian functions. However, the Von Mises stresses are piecewise constant on each cell. Therefore, we modify our plotting routine slightly. The first thing we notice is that we  now set values for each cell, which has a one to one correspondence with the degrees of freedom in the function space.
+# In the previous sections, we have only visualized first order Lagrangian functions. However, the Von Mises stresses are piecewise constant on each cell. Therefore, we modify our plotting routine slightly. The first thing we notice is that we now set values for each cell, which has a one to one correspondence with the degrees of freedom in the function space.
+#
 
 warped.cell_data["VonMises"] = stresses.x.petsc_vec.array
 warped.set_active_scalars("VonMises")
