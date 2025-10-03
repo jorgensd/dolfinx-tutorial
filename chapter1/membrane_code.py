c5825d06d9f115f161daa32dfe3a6512f18d06eb
jorgensd <dokken92@gmail.com>
2025-10-03 23:09:02 +0200

    Update membrane code with crosslinks

--- chapter1/membrane_code.py
+++ chapter1/membrane_code.py
@@ -19,15 +19,16 @@
 # In this section, we will solve the deflection of the membrane problem.
 # After finishing this section, you should be able to:
 # - Create a simple mesh using the GMSH Python API and load it into DOLFINx
-# - Create constant boundary conditions using a geometrical identifier
-# - Use `ufl.SpatialCoordinate` to create a spatially varying function
-# - Interpolate a `ufl.Expression` into an appropriate function space
-# - Evaluate a `dolfinx.fem.Function` at any point $x$
+# - Create constant boundary conditions using a {py:func}`geometrical identifier<dolfinx.fem.locate_dofs_geometrical>`
+# - Use {py:class}`ufl.SpatialCoordinate` to create a spatially varying function
+# - Interpolate a {py:class}`ufl-Expression<ufl.core.expr.Expr>` into an appropriate function space
+# - Evaluate a {py:class}`dolfinx.fem.Function` at any point $x$
 # - Use Paraview to visualize the solution of a PDE
 #
 # ## Creating the mesh
 #
-# To create the computational geometry, we use the Python-API of [GMSH](https://gmsh.info/). We start by importing the gmsh-module and initializing it.
+# To create the computational geometry, we use the Python-API of [GMSH](https://gmsh.info/).
+# We start by importing the gmsh-module and initializing it.
 
 # +
 import gmsh
@@ -35,42 +36,53 @@ import gmsh
 gmsh.initialize()
 # -
 
-# The next step is to create the membrane and start the computations by the GMSH CAD kernel, to generate the relevant underlying data structures. The first arguments of `addDisk` are the x, y and z coordinate of the center of the circle, while the two last arguments are the x-radius and y-radius.
+# The next step is to create the membrane and start the computations by the GMSH CAD kernel,
+# to generate the relevant underlying data structures.
+# The first arguments of `addDisk` are the x, y and z coordinate of the center of the circle,
+# while the two last arguments are the x-radius and y-radius.
 
 membrane = gmsh.model.occ.addDisk(0, 0, 0, 1, 1)
 gmsh.model.occ.synchronize()
 
-# After that, we make the membrane a physical surface, such that it is recognized by `gmsh` when generating the mesh. As a surface is a two-dimensional entity, we add `2` as the first argument, the entity tag of the membrane as the second argument, and the physical tag as the last argument. In a later demo, we will get into when this tag matters.
+# After that, we make the membrane a physical surface, such that it is recognized by `gmsh` when generating the mesh.
+# As a surface is a two-dimensional entity, we add `2` as the first argument,
+# the entity tag of the membrane as the second argument, and the physical tag as the last argument.
+# In a later demo, we will get into when this tag matters.
 
 gdim = 2
 gmsh.model.addPhysicalGroup(gdim, [membrane], 1)
 
-# Finally, we generate the two-dimensional mesh. We set a uniform mesh size by modifying the GMSH options.
+# Finally, we generate the two-dimensional mesh.
+# We set a uniform mesh size by modifying the GMSH options.
 
 gmsh.option.setNumber("Mesh.CharacteristicLengthMin", 0.05)
 gmsh.option.setNumber("Mesh.CharacteristicLengthMax", 0.05)
 gmsh.model.mesh.generate(gdim)
 
 # # Interfacing with GMSH in DOLFINx
-# We will import the GMSH-mesh directly from GMSH into DOLFINx via the `dolfinx.io.gmsh` interface.
+# We will import the GMSH-mesh directly from GMSH into DOLFINx via the {py:mod}`dolfinx.io.gmsh` interface.
 # The {py:mod}`dolfinx.io.gmsh` module contains two functions
 # 1. {py:func}`model_to_mesh<dolfinx.io.gmsh.model_to_mesh>` which takes in a `gmsh.model`
 #   and returns a {py:class}`dolfinx.io.gmsh.MeshData` object.
 # 2. {py:func}`read_from_msh<dolfinx.io.gmsh.read_from_msh>` which takes in a path to a `.msh`-file
 #  and returns a {py:class}`dolfinx.io.gmsh.MeshData` object.
 #
-# The `MeshData` object will contain a `dolfinx.mesh.Mesh`, under the attribute `mesh`.
+# The {py:class}`MeshData` object will contain a {py:class}`dolfinx.mesh.Mesh`,
+# under the attribute {py:attr}`mesh<dolfinx.io.gmsh.MeshData.mesh>`.
 # This mesh will contain all GMSH Physical Groups of the highest topological dimension.
 # ```{note}
 # If you do not use `gmsh.model.addPhysicalGroup` when creating the mesh with GMSH, it can not be read into DOLFINx.
 # ```
-# The `MeshData` object can also contain tags for all other `PhysicalGroups` that has been added to the mesh,
-# that being `vertex_tags`, `edge_tags`, `facet_tags` and `cell_tags`.
+# The {py:class}`MeshData<dolfinx.io.gmsh.MeshData>` object can also contain tags for
+# all other `PhysicalGroups` that has been added to the mesh, that being
+# {py:attr}`cell_tags<dolfinx.io.gmsh.MeshData.cell_tags>`, {py:attr}`facet_tags<dolfinx.io.gmsh.MeshData.facet_tags>`,
+# {py:attr}`ridge_tags<dolfinx.io.gmsh.MeshData.ridge_tags>` and
+# {py:attr}`peak_tags<dolfinx.io.gmsh.MeshData.peak_tags>`.
 # To read either `gmsh.model` or a `.msh`-file, one has to distribute the mesh to all processes used by DOLFINx.
 # As GMSH does not support mesh creation with MPI, we currently have a `gmsh.model.mesh` on each process.
 # To distribute the mesh, we have to specify which process the mesh was created on,
 # and which communicator rank should distribute the mesh.
-# The `model_to_mesh` will then load the mesh on the specified rank,
+# The {py:func}`model_to_mesh<dolfinx.io.gmsh.model_to_mesh>` will then load the mesh on the specified rank,
 # and distribute it to the communicator using a mesh partitioner.
 
 # +
@@ -95,8 +107,10 @@ V = fem.functionspace(domain, ("Lagrange", 1))
 # -
 
 # ## Defining a spatially varying load
-# The right hand side pressure function is represented using `ufl.SpatialCoordinate` and two constants, one for $\beta$ and one for $R_0$.
+# The right hand side pressure function is represented using {py:class}`ufl.SpatialCoordinate` and two constants,
+# one for $\beta$ and one for $R_0$.
 
+# +
 import ufl
 from dolfinx import default_scalar_type
 
@@ -104,9 +118,14 @@ x = ufl.SpatialCoordinate(domain)
 beta = fem.Constant(domain, default_scalar_type(12))
 R0 = fem.Constant(domain, default_scalar_type(0.3))
 p = 4 * ufl.exp(-(beta**2) * (x[0] ** 2 + (x[1] - R0) ** 2))
+# -
 
 # ## Create a Dirichlet boundary condition using geometrical conditions
-# The next step is to create the homogeneous boundary condition. As opposed to the [first tutorial](./fundamentals_code.ipynb) we will use `dolfinx.fem.locate_dofs_geometrical` to locate the degrees of freedom on the boundary. As we know that our domain is a circle with radius 1, we know that any degree of freedom should be located at a coordinate $(x,y)$ such that $\sqrt{x^2+y^2}=1$.
+# The next step is to create the homogeneous boundary condition.
+# As opposed to the [first tutorial](./fundamentals_code.ipynb) we will use
+# {py:func}`locate_dofs_geometrical<dolfinx.fem.locate_dofs_geometrical>` to locate the degrees of freedom on the boundary.
+# As we know that our domain is a circle with radius 1, we know that any degree of freedom should be
+# located at a coordinate $(x,y)$ such that $\sqrt{x^2+y^2}=1$.
 
 # +
 import numpy as np
@@ -119,7 +138,9 @@ def on_boundary(x):
 boundary_dofs = fem.locate_dofs_geometrical(V, on_boundary)
 # -
 
-# As our Dirichlet condition is homogeneous (`u=0` on the whole boundary), we can initialize the `dolfinx.fem.dirichletbc` with a constant value, the degrees of freedom and the function space to apply the boundary condition on.
+# As our Dirichlet condition is homogeneous (`u=0` on the whole boundary), we can initialize the
+# {py:class}`dolfinx.fem.DirichletBC` with a constant value, the degrees of freedom and the function
+# space to apply the boundary condition on. We use the constructor {py:func}`dolfinx.fem.dirichletbc`.
 
 bc = fem.dirichletbc(default_scalar_type(0), boundary_dofs, V)
 
@@ -139,8 +160,13 @@ problem = LinearProblem(
 )
 uh = problem.solve()
 
-# ## Interpolation of a `ufl`-expression
-# As we previously defined the load `p` as a spatially varying function, we would like to interpolate this function into an appropriate function space for visualization. To do this we use the `dolfinx.Expression`. The expression takes in any `ufl`-expression, and a set of points on the reference element. We will use the interpolation points of the space we want to interpolate in to.
+# ## Interpolation of a UFL-expression
+# As we previously defined the load `p` as a spatially varying function,
+# we would like to interpolate this function into an appropriate function space for visualization.
+# To do this we use the class {py:class}`Expression<dolfinx.fem.Expression>`.
+# The expression takes in any UFL-expression, and a set of points on the reference element.
+# We will use the {py:attr}`interpolation points<dolfinx.fem.FiniteElement.interpolation_points>`
+# of the space we want to interpolate in to.
 # We choose a high order function space to represent the function `p`, as it is rapidly varying in space.
 
 Q = fem.functionspace(domain, ("Lagrange", 5))
@@ -155,15 +181,12 @@ pressure.interpolate(expr)
 from dolfinx.plot import vtk_mesh
 import pyvista
 
-pyvista.start_xvfb(0.1)
-# -
-
 # Extract topology from mesh and create pyvista mesh
 
 topology, cell_types, x = vtk_mesh(V)
 grid = pyvista.UnstructuredGrid(topology, cell_types, x)
 
-# Set deflection values and add it to plotter
+# Set deflection values and add it to plotterss
 
 # +
 grid.point_data["u"] = uh.x.array
